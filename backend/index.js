960518c6229a3cfae129747e082ec58a567532c2
TanKittipat <kittipat12tan@gmail.com>
2025-01-16 12:11:42 +0700

    Create backend

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const port = process.env.PORT;
+const db_url = process.env.DB_URL;
+const frontend_url = process.env.FRONTEND_URL;
+
+// Create app
+const app = express();
+
+// Connect to database
+try {
+  mongoose.connect(db_url);
+  console.log("Connected to database successfully!");
+} catch (error) {
+  console.log("Something error occurred while connect DB: " + error);
+}
+
+// Allow frontend access
+app.use(cors({ origin: frontend_url, credentials: true }));
+
+// Show landing page
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, ecommerce!</h1>");
+});
+
+// Make app use json
+app.use(express.json());
+
+// Make app listen on selected port
+app.listen(port, () => {
+  console.log("Listening on: http://localhost:" + port);
+});
